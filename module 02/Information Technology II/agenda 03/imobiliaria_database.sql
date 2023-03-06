402618f127d1645324858f5a82377d8c6c14ea62
beatriz <100150308+beatrixiez@users.noreply.github.com>
2023-03-06 03:43:36 -0300

    new activitie: march 06

---
+++ module 02/Information Technology II/agenda 03/imobiliaria_database.sql
@@ -0,0 +1,52 @@
+USE imobiliaria;
+
+CREATE TABLE condominio (
+    codigo INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    logradouro VARCHAR(50) NOT NULL,
+    numero VARCHAR(10),
+    bairro VARCHAR(30) NOT NULL,
+    cidade VARCHAR(40) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    cep VARCHAR(9) NOT NULL
+);
+
+CREATE TABLE apartamento (
+    numero INT UNSIGNED NOT NULL PRIMARY KEY,
+    tipo ENUM('Padrão', 'Cobertura'),
+    cod_cond INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_apart_cond FOREIGN KEY (cod_cond)
+        REFERENCES condominio (codigo)
+);
+
+CREATE TABLE proprietario (
+    rg INT UNSIGNED NOT NULL PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    telefone INT NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE garagem (
+    numero INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tipo ENUM('Padrão', 'Coberta'),
+    num_apart INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_garag_apart FOREIGN KEY (num_apart)
+        REFERENCES apartamento (numero)
+);
+
+CREATE TABLE sindico (
+    matricula INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    logradouro VARCHAR(50) NOT NULL,
+    numero VARCHAR(10),
+    bairro VARCHAR(30) NOT NULL,
+    cidade VARCHAR(40) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    telefone INT NOT NULL,
+    cod_cond INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_sind_cond FOREIGN KEY (cod_cond)
+        REFERENCES condominio (codigo)
+);
+
+
